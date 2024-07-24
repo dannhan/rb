60f504bf9ec81119ad04ecc4ac1d0fdb9953cf8d
Ahmad Danun Hanani <ahmadanun1@gmail.com>
2024-07-25 01:45:24 +0700

    move page title to header

--- src/components/blocks/header.tsx
+++ src/components/blocks/header.tsx
@@ -1,4 +1,3 @@
-import { SearchInput } from "./search-input";
 import { ModeToggle } from "./mode-toggle";
 import { SidebarMobile } from "./sidebar-mobile";
 
@@ -6,7 +5,9 @@ export function Header() {
   return (
     <header className="flex h-14 items-center gap-4 border-b bg-muted/40 px-4 lg:h-[60px] lg:px-6">
       <SidebarMobile />
-      <SearchInput />
+      {/* <SearchInput /> */}
+
+      <h1 className="w-full text-lg font-semibold md:text-2xl">Inventory</h1>
       <ModeToggle />
     </header>
   );
