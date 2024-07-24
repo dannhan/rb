97fc85f45317310e001f418b15e2af1761c41536
Ahmad Danun Hanani <ahmadanun1@gmail.com>
2024-07-25 01:46:53 +0700

    formatting

--- src/components/login-form.tsx
+++ src/components/login-form.tsx
@@ -15,35 +15,33 @@ export function LoginForm() {
   const [state, formAction] = useFormState(login, { message: "" });
 
   return (
-      <form action={formAction} className="space-y-4">
-        <div className="relative">
-          <Label className="sr-only" htmlFor="password">
-            Password
-          </Label>
-          <Input
-            id="password"
-            name="password"
-            placeholder="Password"
-            type="password"
-            required
-          />
-          <p className="pt-1 text-destructive">{state.message}</p>
+    <form action={formAction} className="space-y-4">
+      <div className="relative">
+        <Label className="sr-only" htmlFor="password">
+          Password
+        </Label>
+        <Input
+          id="password"
+          name="password"
+          placeholder="Password"
+          type="password"
+          required
+        />
+        <p className="pt-1 text-destructive">{state.message}</p>
 
-          {/* Show Password Button */}
-          <Button
-            className="absolute bottom-1 right-1 hidden h-7 w-7"
-            size="icon"
-            variant="ghost"
-            type="button"
-          >
-            <Icons.EyeIcon className="h-4 w-4" />
-            <span className="sr-only">Toggle password visibility</span>
-          </Button>
-        </div>
+        {/* Show Password Button */}
+        <Button
+          className="absolute bottom-1 right-1 hidden h-7 w-7"
+          size="icon"
+          variant="ghost"
+          type="button"
+        >
+          <Icons.EyeIcon className="h-4 w-4" />
+          <span className="sr-only">Toggle password visibility</span>
+        </Button>
+      </div>
 
-        <SubmitButton className="w-full">
-          Sign In
-        </SubmitButton>
-      </form>
+      <SubmitButton className="w-full">Sign In</SubmitButton>
+    </form>
   );
 }
