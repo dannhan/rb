67e8028016487383f7fcfec1ff1ee40a069ff332
Ahmad Danun Hanani <ahmadanun1@gmail.com>
2024-07-29 22:18:01 +0700

    refactor project route

--- src/app/[project]/layout.tsx
+++ src/app/[project]/layout.tsx
@@ -1,8 +1,19 @@
+import { notFound } from "next/navigation";
+import { projectConfig } from "@/config/project";
 import { Dashboard } from "@/components/dashboard";
 
-export default function Layout({ children }: { children: React.ReactNode }) {
+const projects = ["banjar", "kediri", "semarang"];
+
+type Props = {
+  children: React.ReactNode;
+  params: { project: string };
+};
+
+export default function Layout({ children, params }: Props) {
+  if (!projects.includes(params.project)) return notFound();
+
   return (
-    <Dashboard>
+    <Dashboard items={projectConfig.sidebarItems}>
       <div className="flex flex-1 items-center justify-center rounded-lg border border-dashed shadow-sm">
         <div className="flex flex-col items-center gap-1 text-center">
           {children}
