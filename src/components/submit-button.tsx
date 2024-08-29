68e45777c4002fc6e60e34ad19445a9d48403c02
dannhan <ahmadanun1@gmail.com>
2024-08-29 21:19:32 +0700

    extend component to receive all button props

--- src/components/submit-button.tsx
+++ src/components/submit-button.tsx
@@ -5,17 +5,22 @@ import { useFormStatus } from "react-dom";
 import { Button } from "@/components/ui/button";
 import { Icons } from "@/components/icons";
 
-type Props = {
+type Props = React.ButtonHTMLAttributes<HTMLButtonElement> & {
   size?: "default" | "sm" | "lg" | "icon" | null;
   className?: string;
-  children?: React.ReactNode;
 };
 
-export function SubmitButton({ size, className, children }: Props) {
+export function SubmitButton({ size, className, children, ...props }: Props) {
   const { pending } = useFormStatus();
 
   return (
-    <Button size={size} type="submit" disabled={pending} className={className}>
+    <Button
+      size={size}
+      type="submit"
+      disabled={pending}
+      className={className}
+      {...props}
+    >
       {pending && (
         <Icons.spinnerIcon className="mr-2 h-4 w-4 animate-spin [animation-duration:1250ms]" />
       )}
