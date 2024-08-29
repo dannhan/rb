51de5b3d0d4ec65b8d513ac4657f4b32614d101a
dannhan <ahmadanun1@gmail.com>
2024-08-30 02:09:03 +0700

    change font size

--- src/app/home/page.tsx
+++ src/app/home/page.tsx
@@ -19,11 +19,11 @@ export default async function Page() {
   return (
     <div className="flex min-h-screen w-full flex-col bg-accent/10 dark:bg-background">
       <Header className="border-none bg-transparent">
-        <h1 className="w-full text-lg font-semibold md:text-xl">Ria Busana</h1>
+        <h1 className="w-full text-2xl font-semibold md:text-xl">Ria Busana</h1>
       </Header>
       <main className="flex flex-1 flex-col items-center gap-4 px-4 pb-8 pt-4 lg:px-6 lg:pb-12 lg:pt-6">
         <section className="flex w-full max-w-screen-xl flex-1 flex-col gap-4 md:max-h-56">
-          <h2 className="text-xl font-semibold">Konstruksi</h2>
+          <h2 className="text-lg font-medium">Konstruksi</h2>
           <div className="grid flex-1 gap-4 md:grid-cols-2 lg:grid-cols-4 lg:gap-8">
             <CreateProjectSheet defaultType="konstruksi" />
             {konstruksiProjects.map((project, index) => (
@@ -33,7 +33,7 @@ export default async function Page() {
         </section>
         <Separator className="my-4 w-full max-w-screen-xl" />
         <section className="flex w-full max-w-screen-xl flex-1 flex-col gap-4 md:max-h-56">
-          <h2 className="text-xl font-semibold">Renovasi</h2>
+          <h2 className="text-lg font-medium">Renovasi</h2>
           <div className="grid flex-1 gap-4 md:grid-cols-2 lg:grid-cols-4 lg:gap-8">
             <CreateProjectSheet defaultType="renovasi" />
             {renovasiProjects.map((project, index) => (
