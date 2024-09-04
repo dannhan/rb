bb8acd88af06b33d6bdf1b250b1c9b578686bc72
dannhan <ahmadanun1@gmail.com>
2024-09-04 19:19:21 +0700

    implement upload file

--- src/app/[project]/gambar-desain/page.tsx
+++ src/app/[project]/gambar-desain/page.tsx
@@ -1,3 +1,25 @@
+"use client";
+
+import { FileUploader } from "@/components/file-uploader";
+import { UploadedFilesCard } from "./uploaded-files-card";
+
 export default function Page() {
-  return <h1>Gambar Design page</h1>;
+  // const { onUpload, progresses, uploadedFiles, isUploading } = useUploadFile(
+  //   "imageUploader",
+  //   { defaultUploadedFiles: [] },
+  // );
+
+  return (
+    <div className="space-y-6">
+      <FileUploader
+        maxFileCount={4}
+        maxSize={4 * 1024 * 1024}
+        disabled={true}
+        // progresses={progresses}
+        // onUpload={onUpload}
+        // disabled={isUploading}
+      />
+      <UploadedFilesCard uploadedFiles={[]} />
+    </div>
+  );
 }
