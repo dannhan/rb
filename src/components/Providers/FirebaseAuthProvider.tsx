d2ad0614b813c08aac43df3c467978c344a90da1
Ahmad Danun Hanani <ahmadanun1@gmail.com>
2025-05-23 19:04:53 +0700

    feat: bundle-analyzer

--- src/components/Providers/FirebaseAuthProvider.tsx
+++ src/components/Providers/FirebaseAuthProvider.tsx
@@ -3,25 +3,38 @@
 import { type ReactNode, useEffect } from "react";
 import { Session } from "next-auth";
 import { useSession } from "next-auth/react";
-import { signInWithCustomToken } from "firebase/auth";
-import { firebaseAuth } from "@/lib/firebase/client";
-
-async function syncFirebaseAuth(session: Session | null) {
-  if (session && session.firebaseToken) {
-    try {
-      await signInWithCustomToken(firebaseAuth, session.firebaseToken);
-    } catch (error) {
-      console.error(`Error signing in with custom token ${error}`);
-    }
-  } else {
-    firebaseAuth.signOut();
-  }
-}
 
 const FirebaseAuthProvider = ({ children }: { children: ReactNode }) => {
   const { data: session } = useSession();
 
   useEffect(() => {
+    const syncFirebaseAuth = async (session: Session | null) => {
+      const { getAuth, signInWithCustomToken } = await import("firebase/auth");
+      const { getApp, getApps, initializeApp } = await import("firebase/app");
+
+      const firebaseConfig = {
+        apiKey: "AIzaSyBN3ItcUbNwvwNTUrhDNyVfdOnwbu1hU9o",
+        authDomain: "ria-busana.firebaseapp.com",
+        projectId: "ria-busana",
+        storageBucket: "ria-busana.appspot.com",
+        messagingSenderId: "1053993262752",
+        appId: "1:1053993262752:web:e0f64d268e25eff00133c5",
+      };
+
+      const app = getApps().length ? getApp() : initializeApp(firebaseConfig);
+      const auth = getAuth(app);
+
+      if (session?.firebaseToken) {
+        try {
+          await signInWithCustomToken(auth, session.firebaseToken);
+        } catch (error) {
+          console.error(`Error signing in with custom token ${error}`);
+        }
+      } else {
+        auth.signOut();
+      }
+    };
+
     syncFirebaseAuth(session);
   }, [session]);
 
--- src/lib/firebase/client.ts
+++ src/lib/firebase/client.ts
@@ -1,10 +1,6 @@
 // Import the functions you need from the SDKs you need
 import { getApp, getApps, initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
-import { getFirestore } from "firebase/firestore";
-import { getStorage } from "firebase/storage";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
@@ -22,5 +18,3 @@ export const firebaseApp = getApps().length
   ? getApp()
   : initializeApp(firebaseConfig);
 export const firebaseAuth = getAuth(firebaseApp);
-export const firebaseFirestore = getFirestore(firebaseApp);
-export const firebaseStorage = getStorage(firebaseApp);
